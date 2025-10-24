2a5dc23b3cd2563348bf54e9e117427c6729753a
380-ka <k.kasai@gk-yhm.net>
2025-10-24 19:03:18 +0900

    1024010

--- lib/stacks/build-iam-stack.ts
+++ lib/stacks/build-iam-stack.ts
@@ -6,7 +6,7 @@ export class BuildIamUpdateStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // 既存の IAM Role を参照（Role名は CloudFormation によって生成されたもの）
+    // 既存の IAM Role を参照
     const codeBuildRole = iam.Role.fromRoleName(this, 'ImportedCodeBuildRole', 'PipelineStack-CodeBuildServiceRoleA9C1F6A8-OThxR01foKH6');
 
     // SQS 作成権限を追加
@@ -17,7 +17,19 @@ export class BuildIamUpdateStack extends Stack {
         "sqs:SetQueueAttributes",
         "sqs:TagQueue"
       ],
-      resources: ["*"] // 必要に応じて制限可能
+      resources: ["*"]
+    }));
+
+    // Lambda 関数作成権限を追加
+    codeBuildRole.addToPrincipalPolicy(new iam.PolicyStatement({
+      actions: [
+        "lambda:CreateFunction",
+        "lambda:UpdateFunctionCode",
+        "lambda:UpdateFunctionConfiguration",
+        "lambda:TagResource",
+        "iam:PassRole" // Lambda に IAM Role を渡すために必要
+      ],
+      resources: ["*"]
     }));
   }
 }
