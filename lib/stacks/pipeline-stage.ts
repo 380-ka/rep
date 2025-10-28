7d21a8da43fdd3d84decfe11cc21827060b3e3d6
380-ka <k.kasai@gk-yhm.net>
2025-10-28 10:35:41 +0900

    1028001

--- lib/stacks/pipeline-stage.ts
+++ lib/stacks/pipeline-stage.ts
@@ -1,26 +1,11 @@
-// lib/stacks/pipeline-stage.ts
 import { Stage, StageProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { StorageStack } from './storage-stack';
-import { Ec2Stack3 } from './ec2-stack3'; 
-import { BuildIamUpdateStack } from './build-iam-stack'; 
-
 
 export class PipelineStage extends Stage {
   constructor(scope: Construct, id: string, props?: StageProps) {
     super(scope, id, props);
 
-    new StorageStack(this, 'StorageStack', {
-      env: { region: process.env.CDK_DEFAULT_REGION },
-    });
-
-    new Ec2Stack3(this, 'Ec2Stack3', {
-      env: { region: process.env.CDK_DEFAULT_REGION },
-    }); 
-
-    new BuildIamUpdateStack(this, 'BuildIamUpdateStack', {
-      env: { region: process.env.CDK_DEFAULT_REGION },
-    }); 
-
+    // すべてのスタックを削除する場合は、ここを空にする
+    // 例: new StorageStack(...) や new Ec2Stack3(...) を削除
   }
 }
