658b356178632318fa103c19d149c482279e59f1
380-ka <k.kasai@gk-yhm.net>
2025-10-28 16:13:51 +0900

    1028011

---
+++ scr/test/math.test.ts
@@ -0,0 +1,12 @@
+
+import { add } from '../src/utils/math';
+
+describe('add function', () => {
+  it('should return the sum of two numbers', () => {
+    expect(add(2, 3)).toBe(5);
+  });
+
+  it('should handle negative numbers', () => {
+    expect(add(-2, -3)).toBe(-5);
+  });
+});
