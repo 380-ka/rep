f89f03422fd5ada6f9e35990d47659b17a36e43c
380-ka <k.kasai@gk-yhm.net>
2025-10-29 11:34:59 +0900

    1029013

--- lib/stacks/pipeline-stage.ts
+++ lib/stacks/pipeline-stage.ts
@@ -2,6 +2,7 @@
 import { Stage, StageProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { StorageStack } from './storage-stack';
+import { Ec2Stack3 } from './ec2-stack3'; 
 
 export class PipelineStage extends Stage {
   constructor(scope: Construct, id: string, props?: StageProps) {
@@ -10,6 +11,10 @@ export class PipelineStage extends Stage {
     new StorageStack(this, 'StorageStack', {
       env: { region: process.env.CDK_DEFAULT_REGION },
     });
+
+    new Ec2Stack3(this, 'Ec2Stack3', {
+      env: { region: process.env.CDK_DEFAULT_REGION },
+    }); 
 
   }
 }
