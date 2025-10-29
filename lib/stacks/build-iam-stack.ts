83b4082b6f100c8a65e09ce07526c13ecf606d98
380-ka <k.kasai@gk-yhm.net>
2025-10-29 18:25:52 +0900

    1029015

--- lib/stacks/build-iam-stack.ts
+++ lib/stacks/build-iam-stack.ts
@@ -6,20 +6,22 @@ export class BuildIamUpdateStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // 既存の IAM Role を参照
     const codeBuildRole = iam.Role.fromRoleName(this, 'ImportedCodeBuildRole', 'PipelineStack-CodeBuildServiceRoleA9C1F6A8-9JGfRJ5yVo8Q');
 
-    // CloudTrail の権限を追加
-    codeBuildRole.addToPolicy(new iam.PolicyStatement({
-      actions: [
-        "cloudtrail:DescribeTrails",
-        "cloudtrail:GetTrailStatus",
-        "cloudtrail:LookupEvents",
-        "cloudtrail:ListTrails",
-        "cloudtrail:GetEventSelectors",
-        "cloudtrail:GetInsightSelectors"
-      ],
-      resources: ["*"]
+    codeBuildRole.attachInlinePolicy(new iam.Policy(this, 'CloudTrailPolicy', {
+      statements: [
+        new iam.PolicyStatement({
+          actions: [
+            "cloudtrail:DescribeTrails",
+            "cloudtrail:GetTrailStatus",
+            "cloudtrail:LookupEvents",
+            "cloudtrail:ListTrails",
+            "cloudtrail:GetEventSelectors",
+            "cloudtrail:GetInsightSelectors"
+          ],
+          resources: ["*"]
+        })
+      ]
     }));
   }
 }
