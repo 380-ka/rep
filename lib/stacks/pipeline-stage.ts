1bec40156dbb9d08d344ffeea37f6f3a5314edd6
380-ka <k.kasai@gk-yhm.net>
2025-10-30 14:12:27 +0900

    1030003

--- lib/stacks/pipeline-stage.ts
+++ lib/stacks/pipeline-stage.ts
@@ -6,6 +6,7 @@ import { Ec2Stack3 } from './ec2-stack3';
 import { BuildIamUpdateStack } from './build-iam-stack'; 
 import { NotificationStack } from './notification_stack';
 import { CloudTrailToS3Stack } from './cloudtrail_to_s3_stack';
+import { CloudFormationFailureNotifierStack } from './cloudtrail_lambda_sns_notifier.ts';
 
 export class PipelineStage extends Stage {
   constructor(scope: Construct, id: string, props?: StageProps) {
@@ -28,6 +29,10 @@ export class PipelineStage extends Stage {
     }); 
 
     new CloudTrailToS3Stack(this, 'CloudTrailToS3Stack', {
+      env: { region: process.env.CDK_DEFAULT_REGION },
+    }); 
+
+    new CloudFormationFailureNotifierStack(this, 'CloudFormationFailureNotifierStack', {
       env: { region: process.env.CDK_DEFAULT_REGION },
     }); 
 
