c7eb5622d6e4be414aa7fd43c8dbcd228a78cf65
380-ka <k.kasai@gk-yhm.net>
2025-10-30 17:27:50 +0900

    1030

---
+++ lib/stacks/build-iam-stack.ts
@@ -0,0 +1,23 @@
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+export class BuildIamUpdateStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const codeBuildRole = iam.Role.fromRoleName(this, 'ImportedCodeBuildRole', 'arn:aws:iam::266735847975:role/PipelineStack-CodeBuildServiceRoleA9C1F6A8-kcmkQV0CIvAF');
+
+    codeBuildRole.addToPrincipalPolicy(new iam.PolicyStatement({
+      actions: [
+         "cloudtrail:DescribeTrails",
+         "cloudtrail:GetTrailStatus",
+         "cloudtrail:LookupEvents",
+         "cloudtrail:ListTrails",
+         "cloudtrail:GetEventSelectors",
+         "cloudtrail:GetInsightSelectors"
+      ],
+      resources: ["*"]
+    }));
+  }
+}
--- lib/stacks/pipeline-stage.ts
+++ lib/stacks/pipeline-stage.ts
@@ -2,12 +2,17 @@
 import { Stage, StageProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { StorageStack } from './storage-stack';
+import { BuildIamUpdateStack } from './build-iam-stack';
 
 export class PipelineStage extends Stage {
   constructor(scope: Construct, id: string, props?: StageProps) {
     super(scope, id, props);
 
     new StorageStack(this, 'StorageStack', {
+      env: { region: process.env.CDK_DEFAULT_REGION },
+    });
+
+    new BuildIamUpdateStack(this, 'BuildIamUpdateStack', {
       env: { region: process.env.CDK_DEFAULT_REGION },
     });
 
